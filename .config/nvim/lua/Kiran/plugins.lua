f3e3e50c4ba5323161ba40c6abb1524bfe1ae894
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-03-14 17:21:29 +0530

    Removed default mappings and media support

--- .config/nvim/lua/Kiran/plugins.lua
+++ .config/nvim/lua/Kiran/plugins.lua
@@ -1,51 +1,48 @@
 --If I save plugins.lua file then run PackerSync
-vim.cmd [[
+vim.cmd([[
     augroup packer_user_config
         autocmd!
         autocmd BufWritePost plugins.lua source <afile> | PackerSync
     augroup end
-]]
+]])
 
 --Plugins
-return require('packer').startup(function(use)
-
+return require("packer").startup(function(use)
     -- Packer and manage Packer?!
-    use 'wbthomason/packer.nvim'
-    use 'nvim-lua/popup.nvim'
+    use("wbthomason/packer.nvim")
+    use("nvim-lua/popup.nvim")
 
     -- colorscheme of choice
     -- use 'folke/tokyonight.nvim'
-    use 'marko-cerovac/material.nvim'
+    use("marko-cerovac/material.nvim")
 
     --nvim completions
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/cmp-nvim-lsp'
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-cmdline")
+    use("hrsh7th/cmp-nvim-lsp")
 
     -- Fuzzy File Finder
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = {'nvim-lua/plenary.nvim'}
-    }
+    use({
+        "nvim-telescope/telescope.nvim",
+        requires = { "nvim-lua/plenary.nvim" },
+    })
 
     -- Git integration
-    use 'lewis6991/gitsigns.nvim'
-    use "tpope/vim-fugitive"
+    use("lewis6991/gitsigns.nvim")
+    use("tpope/vim-fugitive")
 
     -- LSP
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/nvim-lsp-installer'
+    use("neovim/nvim-lspconfig")
+    use("williamboman/nvim-lsp-installer")
 
     --Tree Sitter
-    use {'nvim-treesitter/nvim-treesitter',
-        run = ":TSUpdate",
-    }
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
-    use 'chrisbra/Colorizer'
+    use("chrisbra/Colorizer")
 
     --Quality of Life
-    use 'sbdchd/neoformat'
-    use 'tpope/vim-surround'
+    use("sbdchd/neoformat")
+    use("tpope/vim-surround")
 end)
