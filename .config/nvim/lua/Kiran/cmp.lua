a336139ae77474ff6815f3059f7adc52b4227566
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-03-14 17:15:55 +0530

    Snippets
    Added simple snippet support for completions.
    Maybe properly configure a snippet engine afterwards.

--- .config/nvim/lua/Kiran/cmp.lua
+++ .config/nvim/lua/Kiran/cmp.lua
@@ -70,6 +70,26 @@ cmp.setup {
             "s",
         }),
     },
+    snippet = {
+        expand = function(args)
+            local line_num, col = unpack(vim.api.nvim_win_get_cursor(0))
+            local line_text = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)[1]
+            local indent = string.match(line_text, "^%s*")
+            local replace = vim.split(args.body, "\n", true)
+            local surround = string.match(line_text, "%S.*") or ""
+            local surround_end = surround:sub(col)
+
+            replace[1] = surround:sub(0, col - 1) .. replace[1]
+            replace[#replace] = replace[#replace] .. (#surround_end > 1 and " " or "") .. surround_end
+            if indent ~= "" then
+                for i, line in ipairs(replace) do
+                    replace[i] = indent .. line
+                end
+            end
+
+            vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, replace)
+        end,
+    },
     formatting = {
         fields = { "kind", "abbr", "menu" },
         format = function(entry, vim_item)
