fdbc3aa2916322630914b3aa4749eb28914246a3
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-06-18 12:27:22 +0530

    Add Some nice keymappings

--- .config/nvim/lua/Kiran/keymaps.lua
+++ .config/nvim/lua/Kiran/keymaps.lua
@@ -15,18 +15,27 @@ keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", opts)
 keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", opts)
 keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", opts)
 keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", opts)
+keymap("n", "<leader>gb", ":Gitsigns blame_line<CR>", opts)
 keymap("n", "<leader>a", ":Lex 30<CR>", opts)
+-- keymap("n", "<leader>o", ":e <C-R>=expand('%:p:h') .. '/' <CR>", opts)
+-- not using lua api because it is not responsive
+vim.cmd[[nnoremap <leader>o :e <C-R>=expand('%:p:h')..'/'<CR>]]
+keymap("n", "<leader>u", "g~w", opts)
+keymap("n", "<leader>fm", ":Neoformat<CR>", opts)
+keymap("n", "<leader>w", ":w<CR>", opts)
+keymap("n", "<leader>Y", '"+y$', opts)
+keymap("n", "<leader>yy", '"+yy', opts)
 keymap("n", "<C-k>", ":resize -2<CR>", opts)
 keymap("n", "<C-j>", ":resize +2<CR>", opts)
 keymap("n", "<C-h>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-l>", ":vertical resize +2<CR>", opts)
 keymap("n", "<C-n>", ":noh<CR>", opts)
-keymap("n", "<leader>gb", ":Gitsigns blame_line<CR>", opts)
 keymap("n", "vs", ":vs<CR>", opts)
 -- }}}
 
 -- Visual Mode mappings {{{
 keymap("v", "<leader>i", "1g<C-a>", opts)
+keymap("v", "<leader>y", '"+y', opts)
 keymap("v", "J", ":m '>+1<CR>gv", opts)
 keymap("v", "K", ":m '<-2<CR>gv", opts)
 keymap("v", "p", '"_dP', opts)
