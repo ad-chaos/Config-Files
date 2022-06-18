da45787087d79a9832eaabc12b823f7fcbaa224d
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-06-18 13:20:46 +0530

    stylua and colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,20 @@
-require "Kiran.options"
-require "Kiran.keymaps"
-require "Kiran.plugins"
-require "Kiran.cmp"
-require "Kiran.treesitter"
-require "Kiran.telescope"
-require "Kiran.lsp"
-require "Kiran.gitsigns"
+require("Kiran.options")
+require("Kiran.keymaps")
+require("Kiran.plugins")
+require("Kiran.cmp")
+require("Kiran.treesitter")
+require("Kiran.telescope")
+require("Kiran.lsp")
+require("Kiran.gitsigns")
+require("Kiran.neovide")
 
 --  Change cursor depending on modes
 --  SI is Insert
 --  SR is Replace
 --  EI is Normal (Else)
-vim.cmd [[let &t_SI.="\e[6 q"]]
-vim.cmd [[let &t_SR.="\e[4 q"]]
-vim.cmd [[let &t_EI.="\e[1 q"]]
+-- vim.cmd [[let &t_SI.="\e[6 q"]]
+-- vim.cmd [[let &t_SR.="\e[4 q"]]
+-- vim.cmd [[let &t_EI.="\e[1 q"]]
 
 -- vim.g.tokyonight_style = "night"
 -- vim.g.tokyonight_italic_functions = true
@@ -25,5 +26,4 @@ vim.cmd([[ set foldmethod=marker ]])
 vim.cmd([[ let g:netrw_banner=0 ]])
 vim.cmd([[ set fo+=/ ]])
 -- Load the colorscheme
-vim.g.material_style = "darker"
-vim.cmd [[colorscheme material]]
+vim.cmd([[ colorscheme dracula ]])
