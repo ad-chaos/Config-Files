0e6e0997534e5e822a19e8404f60331881c5275d
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-08-14 17:02:08 +0530

    links surround

--- .config/nvim/after/ftplugin/markdown.lua
+++ .config/nvim/after/ftplugin/markdown.lua
@@ -1,16 +1,30 @@
 local surround = require "nvim-surround"
-surround.buffer_setup {
+
+surround.buffer_setup({
     surrounds = {
-        ["l"] = {
+        l = {
             add = function()
+                local clipboard = vim.fn.getreg("+"):gsub("\n", "")
                 return {
-                    "[",
-                    "](" .. vim.fn.getreg "*" .. ")",
+                    { "[" },
+                    { "](" .. clipboard .. ")" },
                 }
             end,
+            find = "%b[]%b()",
+            delete = "^(%[)().-(%]%b())()$",
+            change = {
+                target = "^()()%b[]%((.-)()%)$",
+                replacement = function()
+                    local clipboard = vim.fn.getreg("+"):gsub("\n", "")
+                    return {
+                        { "" },
+                        { clipboard },
+                    }
+                end,
+            },
         },
     },
-}
+})
 
 vim.cmd "setlocal spell spelllang=en_US"
 vim.cmd "setlocal textwidth=120"
