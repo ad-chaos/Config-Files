d5c6f391c0f4bea78d4e94316c17549b3f6d971c
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-08-16 20:56:06 +0530

    spelling mistake

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,7 +8,7 @@ require "nvim-lua.options"
 require "nvim-lua.neovide"
 
 vim.cmd [[
-augroup overides
+augroup overrides
     au!
     au ColorScheme dracula hi SpellBad   gui=undercurl
                        \ | hi SpellCap   gui=undercurl
@@ -20,3 +20,12 @@ augroup END
 
 vim.cmd [[ let g:netrw_banner=0 ]]
 vim.cmd [[ colorscheme dracula ]]
+vim.g.loaded_matchit=1
+
+if vim.g.neovide then
+    vim.keymap.set("n", "<D-v>", "\"+p")
+end
+-- local ts_utils = require("nvim-treesitter.ts_utils")
+-- local node = ts_utils.get_node_at_cursor()
+-- local prev_node = ts_utils.get_previous_node(node, true, true)
+-- vim.keymap.set("n", "[p", ts_utils.goto_node(prev_node, true, true))
