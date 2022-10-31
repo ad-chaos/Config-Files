f738363dc8cf12091f39e5d4f222a45e9dabc4ad
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-10-31 21:28:57 +0530

    Install instead of Sync
    Because Syncing might introduce breaking changes that would require
    updating nvim to latest HEAD and that's not nice

--- .config/nvim/lua/nvim-lua/plugins.lua
+++ .config/nvim/lua/nvim-lua/plugins.lua
@@ -2,7 +2,7 @@
 vim.cmd [[
     augroup packer_user_config
         autocmd!
-        autocmd BufWritePost plugins.lua source <afile> | PackerSync
+        autocmd BufWritePost plugins.lua source <afile> | PackerInstall
     augroup end
 ]]
 
