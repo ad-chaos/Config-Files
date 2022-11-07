d721656a27f0d891dc74dba05fefad8858295e81
Kiran-raj <santoshrajpurohit89@gmail.com>
2022-11-07 20:07:24 +0530

    No need to pass in capabilities
    It is redundant since the passed table is used for overriding known
    capabilities.

--- .config/nvim/lua/nvim-lua/lsp/handlers.lua
+++ .config/nvim/lua/nvim-lua/lsp/handlers.lua
@@ -65,10 +65,8 @@ M.on_attach = function(client, bufnr)
     lsp_keymaps(bufnr)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 local cmp_nvim_lsp = require "cmp_nvim_lsp"
 
-M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
