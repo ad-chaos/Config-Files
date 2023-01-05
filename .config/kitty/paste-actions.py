30e42f3a79d7cf0144841ed854ca228019f17fe9
Kiran-raj <santoshrajpurohit89@gmail.com>
2023-01-05 19:50:17 +0530

    more efficient?

--- .config/kitty/paste-actions.py
+++ .config/kitty/paste-actions.py
@@ -5,14 +5,23 @@ def filter_paste(text: str) -> str:
     else it'll be considered the remote name
     """
 
-    github_link = text.find("github.com")
-    if github_link > 0:
-        project = text[github_link:].split("/")
-        match (len(project), project[-1]):
-            case (3, _):
-                return f"git clone {text} && cd {project[-1]}"
-            case (4, "i"):
-                return text[:-1]
-            case (4, remote_name):
-                return f"git remote add {remote_name} {text}"
-    return text
+    if (github_link := text.find("github.com")) == -1:
+        return text
+
+    link, _, modifier = text.rpartition("/")
+    parts = text[github_link:].count("/") + 1
+    match (parts, modifier):
+        case (3, project_name):
+            return f"git clone {text} && cd {project_name}"
+        case (4, "i"):
+            return text[:-1]
+        case (4, remote_name):
+            return f"git remote add {remote_name} {link}"
+
+    assert False, "unreachable"
+
+# Tests
+if __name__ == "__main__":
+    print(filter_paste("https://github.com/org/user"))
+    print(filter_paste("https://github.com/org/user/i"))
+    print(filter_paste("https://github.com/org/user/a_remote"))
