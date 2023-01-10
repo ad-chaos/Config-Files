f870dd9b18b6023d6155c4966dd78b7f5ef4dfc3
Kiran-raj <santoshrajpurohit89@gmail.com>
2023-01-10 16:22:22 +0530

    Clean up install script
    Don't know if it works though

--- install.sh
+++ install.sh
@@ -15,34 +15,31 @@ fi
 link_config() {
     if [ $2 ]; then
         if [[ "$2" =~ ^[Oo]$ ]]; then
-            echo "Deleted .$1"
-            rm ~/.$1
-        elif [[ "$2" =~ ^[Rr]$ ]]; then
-            echo "Renamed and Retained"
-            mv ~/.$1 $1_old
+            echo "Overwrote $1"
+            rm ~/$1
         else
-            echo "Invalid Selection. Please Try again:"
-            check_and_link $1
+            echo "Retaining"
+            mv ~/$1 $1_old
         fi
     fi
 
-    ln -s $current_dir/.$1 ~/.$1
+    ln -s "$current_dir/$1" "$HOME/$1"
     echo "linked $1"
 }
 
 check_and_link() {
-if [[ -f ~/.$1 ]]; then
-    read "choice?Found an existing $1 config file [O]verwrite/[R]etain: "
-    link_config $1 $choice
-else
-    link_config $1
-fi
+    if [[ -f ~/.$1 ]]; then
+        read "choice?Found an existing $1 file [O]verwrite/[R]etain (defaults to retaining): "
+        link_config $1 $choice
+    else
+        link_config $1
+    fi
 }
-# Sym-Linking
-check_and_link zshrc
-check_and_link vimrc
-check_and_link config/nvim
-check_and_link config/kitty
 
-echo "Installation Done"
-echo "Please restart kitty and run PackerSync after opening Neovim"
+# Sym-Linking
+check_and_link ".zshrc"
+check_and_link ".vimrc"
+check_and_link ".config/nvim"
+check_and_link ".config/kitty"
+
+echo "Done"
