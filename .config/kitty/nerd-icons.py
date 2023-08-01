2cf2bf00cd662c008e13c4c660cbe16f1a3c65df
ad_chaos <santoshrajpurohit89@gmail.com>
2023-08-01 18:26:57 +0530

    More Terse

--- .config/kitty/nerd-icons.py
+++ .config/kitty/nerd-icons.py
@@ -1,42 +1,53 @@
-from itertools import pairwise, starmap, groupby, chain
+from collections.abc import Iterator
+from itertools import starmap, groupby
+from more_itertools import first, last, consecutive_groups
 from functools import partial
 import requests
 
 fst = lambda f, _: f
 snd = lambda _, s: s
+sub = lambda val: val[1] - val[0]
 hexint = partial(int, base=16)
-codepoint = lambda code: hex(code)[2:]
-diff1 = lambda val: val[1] - val[0] < 2
+codepoint = lambda code: hex(code)[2:].upper()
+
+
+def json_to_int_ranges(json_data) -> Iterator[tuple[int, int | None]]:
+    return map(
+        lambda group: (first(group), last(group, None)),
+        consecutive_groups(
+            starmap(
+                fst,
+                groupby(
+                    sorted(
+                        map(
+                            hexint,
+                            starmap(
+                                snd,
+                                map(dict.values, json_data.values()),
+                            ),
+                        )
+                    ),
+                ),
+            )
+        ),
+    )
+
 
 glyph_names = requests.get(
     "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/glyphnames.json"
 ).json()
+version = glyph_names["METADATA"]["version"]
 del glyph_names["METADATA"]
 
-codes = starmap(
-    fst,
-    groupby(
-        sorted(
-            map(
-                hexint,
-                starmap(
-                    snd,
-                    map(dict.values, glyph_names.values()),
-                ),
-            )
-        ),
-    ),
-)
-
-ranges = []
-for is_range, group in groupby(pairwise(codes), diff1):
-    if is_range:
-        first, *_, last = chain.from_iterable(group)
-        ranges.append(f"U+{codepoint(first)}-U+{codepoint(last)}")
-    else:
-        next(group)
-        ranges.extend(f"U+{codepoint(fst(*t))}" for t in group)
-
-
 with open("nerd-icons.conf", "w") as f:
-    f.write(f"symbol_map {','.join(ranges)} Symbols Nerd Font Mono\n")
+    f.write(f"# Generated for Nerd font version: {version}\n")
+    f.write(f"symbol_map ")
+    f.write(
+        ",".join(
+            starmap(
+                lambda start, stop: f"U+{codepoint(start)}{f'-U+{codepoint(stop)}' if stop else ''}",
+                json_to_int_ranges(glyph_names),
+            )
+        )
+    )
+    f.write(" Symbols Nerd Font Mono\n")
