67bbf12d6410b1dcbc0a6abc3dc2df4549f7445f
ad-chaos <santoshrajpurohit89@gmail.com>
2024-02-11 12:19:37 +0530

    update nerd fonts

--- .config/kitty/nerd-icons.py
+++ .config/kitty/nerd-icons.py
@@ -1,8 +1,11 @@
+# ruff: noqa: E731
+
 from collections.abc import Iterator
 from itertools import starmap, groupby
 from more_itertools import first, last, consecutive_groups
 from functools import partial
 import requests
+import sys
 
 fst = lambda f, _: f
 snd = lambda _, s: s
@@ -10,29 +13,54 @@ sub = lambda val: val[1] - val[0]
 hexint = partial(int, base=16)
 codepoint = lambda code: hex(code)[2:].upper()
 
+GlyphData = dict[str, dict[str, str]]
+IntRange = tuple[int, int | None]
+
+
+def json_to_int(json_data: GlyphData) -> Iterator[int]:
+    return map(
+        hexint,
+        starmap(
+            snd,
+            map(dict.values, json_data.values()),
+        ),
+    )
+
 
 def json_to_int_ranges(
-    json_data: dict[str, dict[str, str]]
-) -> Iterator[tuple[int, int | None]]:
+    json_data: GlyphData,
+) -> Iterator[IntRange]:
     return map(
         lambda group: (first(group), last(group, None)),
         consecutive_groups(
             starmap(
                 fst,
                 groupby(
-                    sorted(
-                        map(
-                            hexint,
-                            starmap(
-                                snd,
-                                map(dict.values, json_data.values()),
-                            ),
-                        )
-                    ),
+                    sorted(json_to_int(json_data)),
                 ),
             )
         ),
     )
+
+
+def generate_file(json_data: GlyphData, version: str):
+    with open("nerd-icons.conf", "w") as f:
+        f.write(f"# Generated for Nerd font version: {version}\n")
+        f.write("symbol_map ")
+        f.write(
+            ",".join(
+                starmap(
+                    lambda start,
+                    stop: f"U+{codepoint(start)}{f'-U+{codepoint(stop)}' if stop else ''}",
+                    json_to_int_ranges(json_data),
+                )
+            )
+        )
+        f.write(" Symbols Nerd Font Mono\n")
+
+
+def print_all(json_data: GlyphData):
+    print(*map(chr, json_to_int(glyph_names)))
 
 
 glyph_names = requests.get(
@@ -41,15 +69,7 @@ glyph_names = requests.get(
 version = glyph_names["METADATA"]["version"]
 del glyph_names["METADATA"]
 
-with open("nerd-icons.conf", "w") as f:
-    f.write(f"# Generated for Nerd font version: {version}\n")
-    f.write(f"symbol_map ")
-    f.write(
-        ",".join(
-            starmap(
-                lambda start, stop: f"U+{codepoint(start)}{f'-U+{codepoint(stop)}' if stop else ''}",
-                json_to_int_ranges(glyph_names),
-            )
-        )
-    )
-    f.write(" Symbols Nerd Font Mono\n")
+if len(sys.argv) > 1:
+    print_all(glyph_names)
+else:
+    generate_file(glyph_names, version)
